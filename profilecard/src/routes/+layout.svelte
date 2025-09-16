179832805fa84c715153ec13e12c04c62a58f5f3
Colin de Groot <colindegroot2004@gmail.com>
2025-09-16 15:10:38 +0200

    flip werkend en basis afmetingen kaartje (mobile first) #6  #4

--- profilecard/src/routes/+layout.svelte
+++ profilecard/src/routes/+layout.svelte
@@ -8,7 +8,33 @@
 <svelte:head>
   <link rel="icon" href={favicon} />
 
- 
+  <style>
+    section {
+      width: 80vw;
+      height: 380px;
+      position: relative;
+
+	  margin: auto;
+
+      transform-style: preserve-3d;
+      transition: transform 0.6s ease-in-out;
+      will-change: transform;
+      perspective: 1000px;
+
+	  background: #393E46;
+	  background: linear-gradient(145deg, rgba(57, 62, 70, 1) 0%, rgba(0, 0, 0, 1) 100%);
+
+      &:hover {
+        transform: rotateX(180deg);
+      }
+    }
+
+    @media (min-width: 700px;) {
+      section {
+        width: 600px;
+      }
+    }
+  </style>
 </svelte:head>
 
 <main>
