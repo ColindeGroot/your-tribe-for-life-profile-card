2c5ff711de32613065205537e4a34effafc3a2fa
Colin de Groot <colindegroot2004@gmail.com>
2025-09-18 16:10:45 +0200

    fav items inladen close #8

--- profilecard/src/routes/+layout.server.js
+++ profilecard/src/routes/+layout.server.js
@@ -1,19 +1,23 @@
-export async function load(){
-    const userData = await fetch ('https://fdnd.directus.app/items/person/149')
-    const userDataResponse = await userData.json()
+export async function load() {
+  const userData = await fetch('https://fdnd.directus.app/items/person/149');
+  const userDataResponse = await userData.json();
 
-    console.log(userDataResponse)
+  console.log(userDataResponse);
 
-    return { user: userDataResponse.data}
+
+  // filter fav items eruit zodat ik deze met een loop kan weergeven in een van de slides
+  // bestaat uit een onderwerp en de content die erbij hoort (subject,content)
+  const favFilters = [
+    ["fav_property", "Property"],
+    ["fav_kitchen", "Gerecht"],
+    ["fav_book_genre", "Genre"],
+    ["fav_country", "Land"],
+    ["fav_coffee", "Koffie"],
+    ["fav_emoji", "Emoji"]
+  ];
+
+  return { user: userDataResponse.data, fav: favFilters };
 }
 
-const favFilters = [
-    ["fav_property",'Property'],
-    ["fav_kitchen",'Gerecht'],
-    ["fav_book_genre",'Genre'],
-    ["fav_country",'Land'],
-    ["fav_coffee",'Koffie'],
-    ["fav_emoji",'Emoji']
-];
 
-console.log(favFilters)
+
--- profilecard/src/routes/+page.svelte
+++ profilecard/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   let { data } = $props();
   const users = data.user;
+  const favFilters = data.fav
 </script>
 
   <div class="card">
@@ -18,16 +19,25 @@
 
       <section class="back-content">
 
-        <div snap-item>
+        <div>
           <p>{users.custom}</p>
         </div>
 
-        <div class="fav-item snap-item">
+        <div class="fav-item">
           <h3>Fav:</h3>
-          <p>fav item</p>
+          <ul>
+            {#each favFilters as [content, subject]}
+              {#if users[content]}
+                <li>
+                    <p><strong>{subject}</strong></p>
+                    <p>{users[content]}</p>
+                </li>
+              {/if}
+            {/each}
+          </ul>
         </div>
 
-        <div class="snap-item">
+        <div class="">
           <p>Nog iets leuks?</p>
         </div>
 
